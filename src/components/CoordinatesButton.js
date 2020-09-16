6f43f91ca63b9ce25cd79ac3fb072ebcec7debc7
pairofAces <karanchauhan16@gmail.com>
2020-09-15 21:53:16 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,16 @@
 // Code CoordinatesButton Component Here
+import React from 'react';
+
+class CoordinatesButton extends React.Component {
+    //needs a clickHandler for the 'click'
+    //the prop is inherited from index.js
+    clickHandler = (e) => {
+        return this.props.onReceiveCoordinates([e.clientX, e.clientY])
+    }
+
+    render() {
+        return <button onClick={this.clickHandler}>Click me!</button>
+    }
+}
+
+export default CoordinatesButton;
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,19 @@
 // Code DelayedButton Component Here
+import React from 'react';
+
+class DelayedButton extends React.Component {
+    //requires a clickHandler
+    //this component will take 2 props, onDelayedClick(function)
+    // and delay (a number)
+    clickHandler = (e) => {
+        //I need to manually persist the event
+        e.persist()
+        setTimeout(this.props.onDelayedClick, this.props.delay, e)
+    }
+
+    render() {
+        return <button onClick={this.clickHandler}>Click me!</button>
+    }
+}
+
+export default DelayedButton;
